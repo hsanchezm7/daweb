1e805f6056c2206400de14df1dedc60e5bb8f84d
Pablo <92580962+Pablohct@users.noreply.github.com>
2026-06-19 15:57:37 +0200

    feat: añadido botón para cerrar notificación

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AuthProvider>
         <App />
-        <Toaster position="bottom-right" richColors />
+        <Toaster position="bottom-right" richColors closeButton />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
